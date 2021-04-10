ce91e2c72ab7f012a929c8c9ef0c76269bed969e
chr-ge <ace26@protonmail.com>
2021-04-10 14:23:57 -0400

    feat: create icons

---
+++ src/components/icons/RemoveAll.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const RemoveAll = () => (
+  <svg
+    stroke='currentColor'
+    fill='currentColor'
+    strokeWidth='0'
+    viewBox='0 0 20 20'
+    height='1em'
+    width='1em'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fillRule='evenodd'
+      d='M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z'
+      clipRule='evenodd'
+    ></path>
+  </svg>
+)
+
+export default RemoveAll
