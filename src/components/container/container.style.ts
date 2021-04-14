f82bcb21360f9d2cc5332f76c0d9a5d854a7c4bb
chr-ge <ace26@protonmail.com>
2021-04-14 11:25:06 -0400

    fix(styles): replace commas with semicolons

--- src/components/container/container.style.ts
+++ src/components/container/container.style.ts
@@ -2,22 +2,22 @@ import styled from '@emotion/styled'
 import { Theme } from '../../types'
 
 export const Grid = styled.div`
-    margin-top: 2rem,
-    min-height: 24rem,
-    gap: 2,
-    grid-template-columns: repeat(1, 5fr),
-    grid-template-areas: "a a b c c" "d d b e e"
+    margin-top: 2rem;
+    min-height: 24rem;
+    gap: 2rem;
+    grid-template-columns: repeat(1, 5fr);
+    grid-template-areas: "a a b c c" "d d b e e";
 `;
 
 export const GridItem = styled.div<{ theme?: Theme }>`
-    grid-area: a,
-    border-width: thin,
-    background-color: ${(p) => (p.theme.columnBgColor)},
-    grid-template-columns: repeat(1, 5fr),
-    grid-template-areas: "a a b c c" "d d b e e"
+    grid-area: a;
+    border-width: thin;
+    background-color: ${(p) => (p.theme.columnBgColor)};
+    grid-template-columns: repeat(1, 5fr);
+    grid-template-areas: "a a b c c" "d d b e e";
 `;
 
 export const Text = styled.p`
-    font-weight: 600,
-    font-size: 0.5em
+    font-weight: 600;
+    font-size: 0.5em;
 `
