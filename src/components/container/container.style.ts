cbe27281cba72d2f9b91f4eeb8f8f95a7656e745
chr-ge <ace26@protonmail.com>
2021-04-14 13:51:21 -0400

    feat(container): made responsive

--- src/components/container/container.style.ts
+++ src/components/container/container.style.ts
@@ -2,22 +2,54 @@ import styled from '@emotion/styled'
 import { Theme } from '../../types'
 
 export const Grid = styled.div`
-    margin-top: 2rem;
-    min-height: 24rem;
-    gap: 2rem;
-    grid-template-columns: repeat(1, 5fr);
-    grid-template-areas: "a a b c c" "d d b e e";
-`;
+  display: grid;
+  margin-top: 1rem;
+  min-height: 24rem;
+  gap: 0.5rem;
+  grid-template-columns: repeat(1, 5fr);
+  grid-template-areas: 'a' 'd' 'b' 'c' 'e';
 
-export const GridItem = styled.div<{ theme?: Theme }>`
-    grid-area: a;
-    border-width: thin;
-    background-color: ${(p) => (p.theme.columnBgColor)};
-    grid-template-columns: repeat(1, 5fr);
-    grid-template-areas: "a a b c c" "d d b e e";
-`;
+  @media (min-width: 600px) {
+    grid-template-columns: repeat(5, 2fr);
+    grid-template-areas: 'a a b c c' 'd d b e e';
+  }
+`
+
+export const GridItemHeaderLeft = styled.div<{ theme?: Theme }>`
+  grid-area: a;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+  border-width: thin;
+  background-color: ${(p) => p.theme.columnBgColor};
+`
+
+export const GridItemHeaderRight = styled.div<{ theme?: Theme }>`
+  grid-area: c;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  border-width: thin;
+  background-color: ${(p) => p.theme.columnBgColor};
+`
+
+export const GridItemCenter = styled.div`
+  grid-area: b;
+  align-self: center;
+`
+
+export const GridItemColumnLeft = styled.div`
+  grid-area: d;
+  border: thin solid #edf2f7;
+`
+
+export const GridItemColumnRight = styled.div`
+  grid-area: e;
+  border: thin solid #edf2f7;
+`
 
 export const Text = styled.p`
-    font-weight: 600;
-    font-size: 0.5em;
+  font-weight: 600;
+  font-size: 1em;
+  margin: 0;
 `
--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,27 +1,116 @@
 import React from 'react'
-import { Grid, GridItem, Text } from './container.style'
-import { Theme } from '../../types'
+import {
+  Grid,
+  GridItemHeaderLeft,
+  GridItemHeaderRight,
+  GridItemCenter,
+  GridItemColumnLeft,
+  GridItemColumnRight,
+  Text,
+} from './container.style'
+import { Option as OptionType, Theme } from '../../types'
+import Column from '../column/column'
+import Option from '../option/option'
+import Button from '../button/button'
+import AddIcon from '../icons/AddIcon'
+import AddAll from '../icons/AddAll'
+import RemoveIcon from '../icons/RemoveIcon'
+import RemoveAll from '../icons/RemoveAll'
 
 interface ContainerProps {
+  current: OptionType
+  select: (option: OptionType) => void
   add: () => void
   remove: () => void
   addAll: () => void
   removeAll: () => void
-  theme?: Theme
+  options: OptionType[]
+  selected: OptionType[]
+  theme: Theme
 }
 
-const Container = ({ theme }: ContainerProps) => {
+const Container = ({
+  current,
+  select,
+  add,
+  addAll,
+  remove,
+  removeAll,
+  options,
+  selected,
+  theme,
+}: ContainerProps) => {
   return (
     <Grid>
-      <GridItem theme={theme}>
+      <GridItemHeaderLeft theme={theme}>
         <Text>Options</Text>
-      </GridItem>
-      <GridItem theme={theme} />
-      <GridItem theme={theme} />
-      <GridItem theme={theme}>
+      </GridItemHeaderLeft>
+      <GridItemColumnLeft theme={theme}>
+        <Column>
+          {options.map((option) => (
+            <Option
+              key={`r-${option.value}`}
+              label={option.label}
+              isSelected={option.value === current?.value}
+              onClick={() => select(option)}
+              onDoubleClick={remove}
+            />
+          ))}
+        </Column>
+      </GridItemColumnLeft>
+      <GridItemCenter>
+        <Button
+          type='button'
+          onClick={add}
+          label='Add'
+          ariaLabel='Add'
+          rightIcon={<AddIcon />}
+          theme={theme}
+        />
+        <Button
+          type='button'
+          label='Add All'
+          ariaLabel='Add All'
+          onClick={addAll}
+          rightIcon={<AddAll />}
+          marginTop='0.5rem'
+          theme={theme}
+        />
+        <Button
+          type='button'
+          label='Remove'
+          ariaLabel='Remove'
+          onClick={add}
+          leftIcon={<RemoveIcon />}
+          marginTop='1.5rem'
+          theme={theme}
+        />
+        <Button
+          type='button'
+          label='Remove All'
+          ariaLabel='Remove All'
+          onClick={removeAll}
+          leftIcon={<RemoveAll />}
+          marginTop='0.5rem'
+          theme={theme}
+        />
+      </GridItemCenter>
+      <GridItemHeaderRight theme={theme}>
         <Text>Selected</Text>
-      </GridItem>
-      <GridItem theme={theme} />
+      </GridItemHeaderRight>
+      <GridItemColumnRight theme={theme}>
+        <Column>
+          {selected.map((option) => (
+            <Option
+              key={`r-${option.value}`}
+              label={option.label}
+              isSelected={option.value === current?.value}
+              onClick={() => select(option)}
+              onDoubleClick={remove}
+            />
+          ))}
+        </Column>
+      </GridItemColumnRight>
     </Grid>
   )
 }
