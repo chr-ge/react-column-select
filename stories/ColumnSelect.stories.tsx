d5495f5b42a61abab99ffa7c929fa46fabe0da6a
chr-ge <ace26@protonmail.com>
2021-04-15 14:04:23 -0400

    stories: create disableKeyboard

--- stories/ColumnSelect.stories.tsx
+++ stories/ColumnSelect.stories.tsx
@@ -6,8 +6,46 @@ import ColumnSelect from './ColumnSelect'
 export default {
   title: 'Example/Column Select',
   component: ColumnSelect,
+  argTypes: {
+    headerBgColor: {
+      control: 'color',
+      table: {
+        category: 'Colors',
+      },
+    },
+    secondary: {
+      control: 'color',
+      table: {
+        category: 'Colors',
+      },
+    },
+    columnBgColor: {
+      control: 'color',
+      table: {
+        category: 'Colors',
+      },
+    },
+    buttonBgColor: {
+      control: 'color',
+      table: {
+        category: 'Colors',
+      },
+    },
+  },
 } as Meta
 
-const Template: Story = (args) => <ColumnSelect {...args} />
+const Template: Story = (args) => {
+  const theme = {
+    headerBgColor: args.headerBgColor ?? '#d6b1ff',
+    secondary: args.secondary ?? '#cfa4ff',
+    textColor: '#000000',
+    columnBgColor: args.columnBgColor ?? '#CBD5E0',
+    buttonBgColor: args.buttonBgColor ?? '#CBD5E0',
+  }
+  return <ColumnSelect theme={theme} {...args} />
+}
 
 export const Default = Template.bind({})
+
+export const DisabledKeyboard = Template.bind({})
+DisabledKeyboard.args = { ...Default.args, disableKeyboard: true }
--- stories/ColumnSelect.tsx
+++ stories/ColumnSelect.tsx
@@ -1,14 +1,36 @@
 import React, { FC } from 'react'
-import ColumnSelectExample from '../examples/ColumnSelectExample'
+import ColumnSelect from '../src'
+import { Theme } from '../src/types'
 import './column_select.css'
 
-const ColumnSelect: FC = () => (
+export interface ColumnSelectProps {
+  theme?: Theme
+  disableKeyboard?: boolean
+}
+
+const options = [
+  { value: 'pawn', label: 'Pawn' },
+  { value: 'bishop', label: 'Bishop' },
+  { value: 'knight', label: 'Knight' },
+  { value: 'rook', label: 'Rook' },
+  { value: 'queen', label: 'Queen' },
+  { value: 'king', label: 'King' },
+]
+
+const ColumnSelectExample: FC<ColumnSelectProps> = ({
+  theme,
+  disableKeyboard,
+}) => (
   <article>
     <section>
       <h2>Column Select</h2>
-      <ColumnSelectExample />
+      <ColumnSelect
+        options={options}
+        theme={theme}
+        disableKeyboard={disableKeyboard}
+      />
     </section>
   </article>
 )
 
-export default ColumnSelect
+export default ColumnSelectExample
