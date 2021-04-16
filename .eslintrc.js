01a506ab57abb757241d63519bf723620f9da408
chr-ge <ace26@protonmail.com>
2021-04-15 20:35:50 -0400

    chore: ignore propTypes warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,30 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: ['plugin:react/recommended'],
-    plugins: ['react', '@typescript-eslint'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {},
-    settings: {
-      react: {
-        version: 'latest',
-      },
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es2021: true,
   },
-};
+  extends: ['plugin:react/recommended'],
+  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+}
