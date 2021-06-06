073ab69cf4cee0e3fc8fcbcf312d7cee81864ae5
chr-ge <ace26@protonmail.com>
2021-06-06 14:42:13 -0400

    fix(storybook): temporarily disable reactDocgen that cause error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(ts|tsx)"
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/addon-a11y'
-  ]
+  typescript: {
+    reactDocgen: 'none',
+  },
 }
